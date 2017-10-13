4a1913c83557c2eeb14e9fe81762efc83be28fb5
Jack <jmaloney.62651@farnborough.ac.uk>
2017-10-13 14:29:43 +0100

    Started adding GUI Elements

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,17 +1,51 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application{
     @Override
     public void start(Stage stage)throws Exception{
-        Pane root = new Pane();
+        BorderPane root = new BorderPane();
 
         Scene scene = new Scene(root, 1024, 768);
+        scene.getStylesheets().add("stylesheet.css");
+
         stage.setTitle("Inventory Management");
         stage.setScene(scene);
         stage.show();
+
+        VBox menuRoot = new VBox();
+
+        MenuBar menu = new MenuBar();
+        Menu fileMenu = new Menu("File");
+        MenuItem chooseFile = new MenuItem("File Path");
+        fileMenu.getItems().addAll(chooseFile);
+        menu.getMenus().addAll(fileMenu);
+        menuRoot.getChildren().add(menu);
+        root.setCenter(menuRoot);
+
+        TabPane tab = new TabPane();
+        Tab stock = new Tab();
+        stock.setText("Stock");
+        stock.setClosable(false);
+        Pane stockPane = new Pane();
+
+        TableView stockTable = new TableView();
+        stockTable.setPrefSize(600,696);
+
+        stockPane.getChildren().add(stockTable);
+        stock.setContent(stockPane);
+
+
+
+        tab.getTabs().addAll(stock);
+        root.setCenter(tab);
+
     }
 
     public static void main(String[] args){
